3bbbf196965e69c8ab95f256dec45437c0325bc5
Tochukwu Franklin Ene <teec3000@gmail.com>
2024-02-14 13:35:05 +0100

    Added the other stakeholders

---
+++ apps/cscs/components/alert.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+    <div>
+        <h1 class="bg-red ">Alert</h1>
+    </div>
+</template>
---
+++ apps/custodian/app.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <NuxtLayout >
+        <div class="flex">
+            <div class="">
+                <sidebarComponent />
+            </div>
+           <div class="w-[100%]">
+            <headerComponent class="relative" />
+            <NuxtPage class="overflow-scroll h-[95vh] pb-3 "/>
+           </div>
+        </div>
+    </NuxtLayout>
+</div>
+</template>
