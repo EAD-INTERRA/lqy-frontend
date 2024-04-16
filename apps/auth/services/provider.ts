381ee7bdd9f4c1a5bb5baf13b355f78fe23bfc5e
Tochukwu Franklin Ene <teec3000@gmail.com>
2024-04-16 17:03:55 +0100

    Hot fix

--- apps/auth/services/provider.ts
+++ apps/auth/services/provider.ts
@@ -9,12 +9,12 @@ export interface ServiceProviderInterface {
 
 export const serviceProvider = (): ServiceProviderInterface => {
     const auth = axios.create({
-        baseURL: 'http://192.168.200.233:5500/api/v1/account/' //TEST
+        baseURL: 'http://131.226.242.233:5500/api/v1/account/' //TEST
         // baseURL: 'http://localhost:3333/api/v1/account', //LOCAL
     })
     const base = axios.create({
         // baseURL: 'http://localhost:3333/api/v1/'
-        baseURL: 'http://192.168.200.233:5600/api/v1/', //TEST
+        baseURL: 'http://131.226.242.233:5600/api/v1/', //TEST
     })
     return {
         auth: new AuthService(auth),
